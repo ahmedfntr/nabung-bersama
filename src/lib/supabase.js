78b4fcad0d98414e811b366bbb8f20b8b9c1c849
ahmedfntr <151494610+ahmedfntr@users.noreply.github.com>
2026-09-10 15:02:17 +0700

    Format supabaseUrl declaration for consistency

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dcqqrpyhryijebqumtpn.supabase.co";
+const supabaseUrl =
+  "https://dcqqrpyhryijebqumtpn.supabase.co";
 
 const supabasePublishableKey =
   "sb_publishable_k7sJi_d-oZqAc3b7A9rYLA_82Wv-pjL";
